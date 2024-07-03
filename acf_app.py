de344ed18d9c6e6bfb3c7c17001321f55c38764e
gluck <glucksistemi@gmail.com>
2024-07-03 07:48:56 +0300

    добавлено краткое руководство

--- acf_app.py
+++ acf_app.py
@@ -3,7 +3,21 @@ import zipfile
 from pathlib import Path
 from acf import transform_acf
 from typing import List
+import os
 
+
+title = "Инструмент для конвертации ACF в XLSX"
+description = """
+Инструкция:
+1. Загрузите один или более файлов в левую часть экрана (перетаскиванием или кликнув и далее выбрав на ПК)
+2. Нажмите Submit
+3. Скачайте результат из правой половины окна (при загрузке нескольких файлов, результаты будут скачиваться в виде zip-архива)
+4. Для загрузки следующей порции файлов, нажмите Clear
+
+Видео-версия инструкции [по ссылке](https://vk.com/video9302191_456239220?list=ln-JuJGnX1lICuKrrmPGz)
+
+Исходный код: [https://github.com/Glucksistemi/leaf_tools](https://github.com/Glucksistemi/leaf_tools)
+"""
 
 def multiple_conv(acf_files: List[str]) -> str:
     result_files = []
@@ -31,8 +45,12 @@ def multiple_conv(acf_files: List[str]) -> str:
 
 app = gr.Interface(
     fn=multiple_conv,
-    inputs=gr.File(file_count='multiple', label='upload one or more .acf files', file_types=['.acf', '.ACF']),
-    outputs=gr.File()
+    inputs=[
+        gr.File(file_count='multiple', label='upload one or more .acf files', file_types=['.acf', '.ACF']),
+    ],
+    outputs=gr.File(),
+    title=title,
+    description=description
 )
 
-app.launch(server_port=7860, server_name="127.0.0.1")
+app.launch(server_port=7860, server_name="127.0.0.1", root_path=os.environ.get('ROOT_PATH', ''))
